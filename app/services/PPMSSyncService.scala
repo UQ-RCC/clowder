3eac19a8b9214ab158e113508d9c3021e0e6933b
hoangnguyen177 <hoangnguyen177@gmail.com>
2021-12-24 14:37:31 +1000

    removing title from search

--- app/services/PPMSSyncService.scala
+++ app/services/PPMSSyncService.scala
@@ -202,7 +202,9 @@ class PPMSSyncService (application: Application) extends Plugin {
     // val allSpaces = spaces.list()
     // Logger.info("There are total of:" + allSpaces.length + " spaces")
     // var spaceList = allSpaces.filter(_space => _space.name == projName)
-    var spaceList = spaces.listAccess(0, projName, Set[Permission](Permission.ViewSpace), getFirstAdmin, showAll=true, showPublic=true, showOnlyShared=false)
+    val allSpaces = spaces.listAccess(0, Set[Permission](Permission.ViewSpace), getFirstAdmin, showAll=true, showPublic=true, showOnlyShared=false)
+    Logger.info("There are total of:" + allSpaces.length + " spaces")
+    var spaceList = allSpaces.filter(_space => _space.name == projName)
     Logger.info("Found:" + spaceList.length + " spaces with given name")
     if (spaceList.length == 0) {
       Logger.info(">>>>>>> No space exists, create a new one <<<<<<")
